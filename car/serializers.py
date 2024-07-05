ecc997268415c94c8642c1bfe9e269df131f4a1e
xboris <boris.tikhonov.21@gmail.com>
2024-07-06 02:01:23 +0300

    fixed flake8

--- car/serializers.py
+++ car/serializers.py
@@ -8,16 +8,25 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1914)])
+    horse_powers = serializers.IntegerField(
+        validators=[MinValueValidator(1),
+                    MaxValueValidator(1914)])
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+    problem_description = serializers.CharField(allow_null=True,
+                                                allow_blank=True,
+                                                required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
-        instance.model = validated_data.get('model', instance.model)
-        instance.horse_powers = validated_data.get('horsepower', instance.horsepower)
-        instance.is_broken = validated_data.get('is_broken', instance.is_broken)
-        instance.problem_description = validated_data.get('problem_description', instance)
+        instance.manufacturer = validated_data.get("manufacturer",
+                                                   instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get("horsepower",
+                                                   instance.horsepower)
+        instance.is_broken = validated_data.get("is_broken",
+                                                instance.is_broken)
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance)
