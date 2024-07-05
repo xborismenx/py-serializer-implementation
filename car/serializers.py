55b4edb809311a9ff94ed9ecf3ef09641b3040f4
xboris <boris.tikhonov.21@gmail.com>
2024-07-06 01:56:57 +0300

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,23 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from rest_framework import serializers
+
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1914)])
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
+        instance.model = validated_data.get('model', instance.model)
+        instance.horse_powers = validated_data.get('horsepower', instance.horsepower)
+        instance.is_broken = validated_data.get('is_broken', instance.is_broken)
+        instance.problem_description = validated_data.get('problem_description', instance)
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import io
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    return Car(**data)
